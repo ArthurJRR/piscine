cd6c3d91a88506926e32fd0f14030f203b3bf72d
ArthurJRR <arthur.rouaix@isep.fr>
2018-02-24 17:38:58 +0100

    Camera and Board

---
+++ Piscine/Assets/Scripts/JeremyTest/Map.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+    public int[,] map1 = new int[9, 9] { { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                         { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                                         { 2, 2, 2, 1, 1, 1, 2, 2, 2 },
+                                         { 2, 2, 2, 2, 1, 2, 2, 2, 2 },
+                                         { 3, 2, 2, 2, 2, 2, 2, 2, 3 },
+                                         { 2, 2, 2, 2, 1, 2, 2, 2, 2 },
+                                         { 2, 2, 2, 1, 1, 1, 2, 2, 2 },
+                                         { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                                         { 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Piscine/Assets/Scripts/JeremyTest/test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public Board board;
+	// Use this for initialization
+	void Start () {
+	    GameObject boardobject = new GameObject("Board");
+        boardobject.transform.parent = transform; //le parent du boardobject  est le transform sur 
+        //lequ'elle est appliquer le scripte ici jeu
+        board = boardobject.AddComponent<Board>(); // ceci ajoute au board (boardobjet) le script Board
+        board.DirtPrefab = (GameObject) Resources.Load("Tile"); //mais le préfab néccéssaire au scripte Boards
+        // dans le gameobject board
+        board.Water1Prefab = (GameObject)Resources.Load("Water1");
+        board.Water0Prefab = (GameObject)Resources.Load("Water0");
+        board.Water2Prefab = (GameObject)Resources.Load("Water2");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
