7a5ddcbd3a92521b9ccc716a2c2bce81e5dbde66
ArthurJRR <arthur.rouaix@isep.fr>
2018-04-24 15:37:02 +0200

    SlideButton, beginning of the card data structure and menu

---
+++ Piscine/Assets/Scripts/Cards/CardData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CardData {
+    public string title;
+    public string image;
+    public string description;
+}
---
+++ Piscine/Assets/Scripts/Cards/DeckData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DeckData  { //A deck represent a group of cards
+    public string deckName;
+    public CardData[] deckCards;
+    public bool isPlayable; //If this boolean is set to true then the deck can be used by a player in a match
+}
