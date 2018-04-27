5e6b338df3c579642506c37e4427ba4849a71a59
ArthurJRR <arthur.rouaix@isep.fr>
2018-04-27 14:52:19 +0200

    Add First Version of Windows Deck Editor

---
+++ Piscine/Assets/Scripts/SelectFactionButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectFactionButton : MonoBehaviour
+{
+
+    public Text buttonText;
+
+    private FactionData factionData;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void Setup(FactionData faction)
+    {
+        this.factionData = faction;
+        buttonText.text = faction.factionName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
